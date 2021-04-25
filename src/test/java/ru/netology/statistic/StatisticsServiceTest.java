bc1fc73ba621745d0aeba2ba8772737657f71a38
Yulia Kobeleva <kobelevaulia@gmail.com>
2021-04-25 21:50:12 +0300

    commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,5 +18,15 @@ class StatisticsServiceTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void findMax2() {
+        StatisticsService service = new StatisticsService();
 
+        long[] incomesInBillions = {12, 5, 8, 4, 18, 3, 8, 6, 11, 11, 12};
+        long expected = 18;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
